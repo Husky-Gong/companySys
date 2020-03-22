92d932a9ff57cbf5f0bd9ad3691e9e490237c067
ZexiGong <gong.ze@husky.neu.edu>
2020-03-22 15:16:05 +0800

    base controller

---
+++ src/main/java/com/zx/common/base/controller/BaseController.java
@@ -0,0 +1,35 @@
+package com.zx.common.base.controller;
+
+import com.zx.common.base.Form;
+import com.zx.common.base.Query;
+import com.zx.common.base.service.IBaseService;
+import org.springframework.web.bind.annotation.*;
+
+public class BaseController<Query,Form> {
+
+    private IBaseService baseService;
+
+    public BaseController(IBaseService baseService){
+        this.baseService = baseService;
+    }
+
+    @GetMapping("page.do")
+    public Object page(Query query){
+        return  baseService.queryPage(query);
+    }
+
+    @RequestMapping("add.do")
+    public Object add(Form form){
+        return baseService.add(form);
+    }
+
+    @RequestMapping("update.do")
+    public Object update(Form form){
+        return baseService.update(form);
+    }
+
+    @RequestMapping("delete.do")
+    public Object delete(Integer id){
+        return baseService.delete(id);
+    }
+}
--- src/main/java/com/zx/sys/controller/SysCompanyController.java
+++ src/main/java/com/zx/sys/controller/SysCompanyController.java
@@ -1,5 +1,6 @@
 package com.zx.sys.controller;
 
+import com.zx.common.base.controller.BaseController;
 import com.zx.sys.form.SysCompanyForm;
 import com.zx.sys.query.SysCompanyQuery;
 import com.zx.sys.service.ISysCompanyService;
@@ -9,18 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 //表示返回的类都是JSON
 @RestController
 @RequestMapping("company")
-public class SysCompanyController {
+public class SysCompanyController extends BaseController<SysCompanyQuery,SysCompanyForm> {
 
     @Autowired
     private ISysCompanyService sysCompanyService;
 
-    @RequestMapping("page.do")
-    public Object page(SysCompanyQuery query){
-        return sysCompanyService.queryPage(query);
-    }
-
-    @RequestMapping("add.do")
-    public Object add(SysCompanyForm form){
-        return sysCompanyService.add(form);
+    public SysCompanyController(ISysCompanyService sysCompanyService){
+        super(sysCompanyService);
     }
 }
